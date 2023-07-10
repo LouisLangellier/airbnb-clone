a96c4200c77645e61930e85be8163d0c9247bc1a
LouisLangellier <louis.langellier@gmail.com>
2023-07-10 10:19:40 +0200

    Auth UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import { Nunito } from "next/font/google";
 
 import './globals.css'
 import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
 
 export const metadata = {
   title: 'Airbnb',
@@ -21,7 +24,12 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body className={font.className}>
-        <Navbar />
+        {/* Reactiver le clientonly si jamais y'a un truc qui marche pas */}
+        {/* <ClientOnly>  */}
+          <ToasterProvider />
+          <RegisterModal />
+          <Navbar />
+        {/* </ClientOnly> */}
         {children}
       </body>
     </html>
