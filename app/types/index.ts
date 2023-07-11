524b675202a38c34338157a92469a3be82859e90
LouisLangellier <louis.langellier@gmail.com>
2023-07-11 11:02:25 +0200

    Social login google and github

---
+++ app/types/index.ts
@@ -0,0 +1,11 @@
+import { User } from "@prisma/client";
+import { CreateAxiosDefaults } from "axios";
+
+export type SafeUser = Omit<
+    User,
+    "createdAt" | "updatedAt" | "emailVerified"
+> & {
+    createdAt: string;
+    updatedAt: string;
+    emailVerified: string | null
+}
